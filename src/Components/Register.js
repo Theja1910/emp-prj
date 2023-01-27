776a7e0acdfcd7d30f6b42060690bbecb06b1085
Theja1910 <123542656+Theja1910@users.noreply.github.com>
2023-01-27 12:48:13 +0530

    Update Register.js
    Lastest commit

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -13,14 +13,50 @@ export default function Register() {
     const [phone, setPhone] = useState("");
     const [address, setAddress] = useState("");
     const [city, setCity] = useState("");
-    //const [stateid,setStateid] = useState("");
+    const [zipcode,setZipcode] = useState("");
     const [password, setPassword] = useState("");
+    const [stateNames,setStateNames]= useState(["TamilNadu","Karnataka","Kerala"]);
+    const [statevalue,setStateValue]= useState("");
+    const [errors,setErrors] = useState({error:false,msg:""});
 
     const data = { empid,fname, lname,email,phone, address,password }
+    const emailvalidation = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     const submitForm = () => {
+     if(empid===""){
+            setErrors({error:true, msg:"Please Fill Your Employee Id"})
+        }
+        else if(fname===""){
+            setErrors({error:true, msg:"Please Fill Your First Name"})
+        }
+        else if(lname===""){
+            setErrors({error:true, msg:"Please Fill Your Last Name"})
+        }
+        else if(email===""){
+            setErrors({error:true, msg:"Please Fill Your Correct Email"})
+        }
+        else if(!emailvalidation.test(email)){
+            setErrors({error:true, msg:"Please Fill Valid Email"})
+        }
+        else if(phone===""){
+            setErrors({error:true, msg:"Please Fill Your Contact Number"})
+        }
+        else if(address===""){
+            setErrors({error:true, msg:"Please Fill Your Address"})
+        }
+        else if(city===""){
+            setErrors({error:true, msg:"Please Fill Your City"})
+        }
+        else if(zipcode===""){
+            setErrors({error:true, msg:"Please Fill Your ZipCode"})
+        }
+        else if(password===""){
+            setErrors({error:true, msg:"Please Fill Your Password"})
+        }
+        else{
         console.log(data, "formdata")
         fetchData("url", "token", data)
+
         setEmpId("")
         setFName("")
         setLname("")
@@ -29,13 +65,20 @@ export default function Register() {
         setAddress("")
         setCity("")
         navigate("/login")
+        }
+        
+
+
 
     }
-    // const handlestate = (e)=>
-    // {
-    //     const stateid = e.target.value;
-    //     setStateid(stateid);
-    // }
+
+    
+    const handlestate = (e)=>
+    {
+        const stateid = e.target.value;
+        setStateValue(stateid);
+        console.log(stateid);
+    }
 
     return (
         <div className="row justify-content-left pt-5">
@@ -84,10 +127,10 @@ export default function Register() {
 
                     <div className="form-group">
                         <label>Contact Number:</label>
-                        <input type="phonenumber" 
+                        <input type="number" min = "1" max="10"
                         className="form-control" 
                         value={phone} 
-                        placeholder="Enter Your Contact"
+                        placeholder="Enter Your Contact Number"
                         onChange={e => setPhone(e.target.value)}
                         id="phonenum" 
                         required />
@@ -115,7 +158,18 @@ export default function Register() {
                         required />
                     </div>
 
-                    {/* <div className="form-group mt-3">
+                    <div className="form-group">
+                        <label>ZipCode</label>
+                        <input type="zipcode" 
+                        className="form-control" 
+                        value={zipcode} 
+                        placeholder="Enter Your ZipCode"
+                        onChange={e => setZipcode(e.target.value)}
+                        id="zipcode" 
+                        required />
+                    </div>
+
+                    <div className="form-group mt-3">
                         <label>State</label>
                         <select name="states" 
                         className="form-control"
@@ -123,14 +177,21 @@ export default function Register() {
                         >
                             <option value="">Select State</option>
                             {
-                                state.map((getstate,index)=>
+                                stateNames.map((name,index)=>
                                 (
-                                    <option value={getstate.state_id} key={index}>{getstate.state_name}</option>
+                                    <option value={name} key={index}>{name}</option>
                                 ))
                             }
-                        </select> */}
+                        </select>
 
-                    
+                    </div>
+                    <div className="form-group mt-3">
+                    <input type="checkbox" 
+                    id="admin" 
+                    name="admin" 
+                    value="admin" />
+                        <label for="vehicle1">admin</label>
+                    </div>
 
                     <div className="form-group mt-3">
                         <label>Password:</label>
@@ -141,6 +202,12 @@ export default function Register() {
                         onChange={e => setPassword(e.target.value)}
                         id="pwd" required />
                     </div>
+
+                    <div className="form-group mt-3">
+                        <div className="errors">
+                            {errors.error === true && (<div className="errors">{errors.msg}</div>)}
+                        </div>
+                        </div>
 
                     
                     <button type="button" 
