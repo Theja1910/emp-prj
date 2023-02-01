3914ece4979591f8bc8c654266854ae7e4ad66ff
Theja1910 <thejaswini.nagaraju@perficient.com>
2023-02-01 10:05:01 +0530

    Adding id in Register

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -42,7 +42,7 @@ export default function Login() {
                 else {
                     toast.warn("Login Failed please enter correct email")
                 }
-                //navigate('/dashboard');
+                
             })
         }
 
