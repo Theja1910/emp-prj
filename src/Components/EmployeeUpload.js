5ccc6c89c2cd7e0e002c78abf8285adcc3edb905
Theja1910 <thejaswini.nagaraju@perficient.com>
2023-02-01 10:13:13 +0530

    Latest Commit

--- src/Components/EmployeeUpload.js
+++ src/Components/EmployeeUpload.js
@@ -1,43 +1,7 @@
 import { useState } from "react";
-//import { fetchData } from "../Auth/Helper";
-// import Papa from "papaparse";
 import "../Components/App.css"
 
 export default function EmployeeUpload() {
-  // const [parsedData, setParsedData] = useState([]);
-
-  //State to store table Column name
-  // const [tableRows, setTableRows] = useState([]);
-
-  //State to store the values
-  // const [values, setValues] = useState([]);
-
-  // const changeHandler = (event) => {
-  //   // Passing file data (event.target.files[0]) to parse using Papa.parse
-  //   Papa.parse(event.target.files[0], {
-  //     header: true,
-  //     skipEmptyLines: true,
-  //     complete: function (results) {
-  //       const rowsArray = [];
-  //       const valuesArray = [];
-
-  //       // Iterating data to get column name and their values
-  //       results.data.map((d) => {
-  //         rowsArray.push(Object.keys(d));
-  //         valuesArray.push(Object.values(d));
-  //       });
-
-  //       // Parsed Data Response in array format
-  //       setParsedData(results.data);
-
-  //       // Filtered Column Names
-  //       setTableRows(rowsArray[0]);
-
-  //       // Filtered Values
-  //       setValues(valuesArray);
-  //     },
-  //   });
-  // };
 
   const [file, setFile] = useState(null);
 
@@ -77,7 +41,8 @@ export default function EmployeeUpload() {
       <br />
       <br />
       <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
+        <input type="file" onChange={handleFileChange} 
+        accept=".csv" />
         <button type="submit">Upload</button>
       </form>
       {/* Table */}
